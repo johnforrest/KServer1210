079b6bf9dd81e87b47dbc70b4d3ee45841a866f2
johnforrest <571716604@qq.com>
2020-04-15 14:08:51 +0800

    0415替换端口号为9081

--- src/app.ts
+++ src/app.ts
@@ -29,7 +29,7 @@ app.all("*", function (req, res, next) {
 });
 
 // 配置服务器
-app.set("port", process.env.PORT || 10095);
+app.set("port", process.env.PORT || 9081);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
